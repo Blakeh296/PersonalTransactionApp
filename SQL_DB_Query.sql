c9318b1d4482e80125ce147a607aa59338cf0c3f
Blakeh296 <bhoelle216@gmail.com>
2019-10-06 14:29:55 -0400

    Added additional forms that will serve as practice and training stock options

--- SQL_DB_Query.sql
+++ SQL_DB_Query.sql
@@ -1,17 +1,22 @@
-SELECT * FROM [TransactionHistory]
-SELECT * FROM [TransactionCategory]
-SELECT * FROM TransactionType
+
+
 
 SELECT * FROM TransactionHistory TH
 inner join TransactionType TT
 on TH.TypeName = TT.TypeName
 WHERE TT.TypeID = 1
 
-/*INSERT INTO TransactionType (TypeName) VALUES ('Withdraw')*/
 
-		/*delete TransactionType
-		where TypeID = '2'*/
+		/*SELECT * FROM [TransactionHistory]
+		SELECT * FROM [TransactionCategory]
+		SELECT * FROM TransactionType
 
-		/*update TransactionHistory
+		INSERT INTO TransactionType (TypeName) VALUES ('Withdraw')
+
+		delete TransactionType
+		where TypeID = '2'
+
+		update TransactionHistory
 		set TransactionDate = '05/23/2019'
 		where TransactionID  = '469'*/
+		
