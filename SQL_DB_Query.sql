34a928fba00691c20e7479f416183a0a41ac7a04
Blakeh296 <bhoelle216@gmail.com>
2019-10-07 16:46:04 -0400

    Practiced manipulating data in the DB without a web form

--- SQL_DB_Query.sql
+++ SQL_DB_Query.sql
@@ -1,5 +1,5 @@
 
-
+/*
 
 SELECT * FROM TransactionHistory TH
 inner join TransactionType TT
@@ -7,16 +7,19 @@ on TH.TypeName = TT.TypeName
 WHERE TT.TypeID = 1
 
 
-		/*SELECT * FROM [TransactionHistory]
+		SELECT * FROM [TransactionHistory]
 		SELECT * FROM [TransactionCategory]
 		SELECT * FROM TransactionType
 
 		INSERT INTO TransactionType (TypeName) VALUES ('Withdraw')
 
-		delete TransactionType
-		where TypeID = '2'
+		delete TransactionHistory
+		where TransactionID = '594'
 
 		update TransactionHistory
-		set TransactionDate = '05/23/2019'
-		where TransactionID  = '469'*/
+		set TransactionDate = '7/24/2019'
+		where TransactionID  = '579'*/
 		
+		INSERT INTO TransactionHistory ([Name], CategoryID, Amount, TypeName, TransactionDate, Notes) 
+		VALUES ('Clash Royale','9','3.01','Withdraw', '8/5/2019',''),('Sams Club','3','3.20','Withdraw', '8/5/2019',''),
+		('Bp 8236333Circle K St','6','0.85','Withdraw', '8/5/2019',''),('Sams Club','3','2.13','Withdraw', '8/4/2019','')
